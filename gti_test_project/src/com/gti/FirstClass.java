7a89d9c2d314387a547e7336faa9cd6e738241d3
tom_glb <tom_glb@163.com>
2019-12-05 20:43:08 +0800

    修改update

--- gti_test_project/src/com/gti/FirstClass.java
+++ gti_test_project/src/com/gti/FirstClass.java
@@ -3,5 +3,6 @@ package com.gti;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first gitpriject");
+        System.out.println("first gitpriject update");
     }
 }
